69b58d7b0d54b1d309c3e6b3ac82e0467984a1c0
Damon Oehlman <damon.oehlman@gmail.com>
2015-04-21 13:21:32 +1000

    Refactor test server to work with broth

--- test/server.js
+++ test/server.js
@@ -2,24 +2,23 @@ var fs = require('fs');
 var path = require('path');
 var http = require('http');
 var mime = require('mime');
-var server = http.createServer();
-var port = process.env.ZUUL_PORT || process.env.PORT;
 
-server.on('request', function(req, res) {
-  var src;
+module.exports = function() {
+  return http.createServer(function(req, res) {
+    var src;
 
-  if (req.url === '/tennis.webm') {
-    src = path.resolve(__dirname, 'assets', 'tennis.webm');
-  }
+    console.log(req.url);
+    if (req.url === '/tennis.webm') {
+      src = path.resolve(__dirname, 'assets', 'tennis.webm');
+    }
 
-  if (src) {
-    res.writeHead(200, { 'Content-Type': mime.lookup(src) });
-    fs.createReadStream(src).pipe(res);
-  }
-  else {
-    res.writeHead(404);
-    res.end();
-  }
-});
-
-server.listen(parseInt(port, 10) || 3000);
+    if (src) {
+      res.writeHead(200, { 'Content-Type': mime.lookup(src) });
+      fs.createReadStream(src).pipe(res);
+    }
+    else {
+      res.writeHead(404);
+      res.end();
+    }
+  });
+};
